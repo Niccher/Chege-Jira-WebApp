f73b7ff98a813d0e6a24cd74ac2bd505a2322679
Niccher <domi777nicch@gmail.com>
2026-09-13 14:06:58 +0300

    fix(config): make baseURL dynamic based on HTTP_HOST for Railway deployments

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     //public string $baseURL = 'http://localhost:8080/';
-    public string $baseURL = 'https://chegeos.chegecache.co.ke/';
+    public string $baseURL = 'http://localhost:8080/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -204,6 +204,12 @@ class App extends BaseConfig
     public function __construct()
     {
         parent::__construct();
+        
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) ? 'https' : 'http';
+            $this->baseURL = $protocol . '://' . $_SERVER['HTTP_HOST'] . '/';
+        }
+        
         $this->baseURL = env('app.baseURL', $this->baseURL);
     }
 }
