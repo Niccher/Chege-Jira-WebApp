4b736930877336a8088b51a7924db5327a816b33
Niccher <domi777nicch@gmail.com>
2026-09-13 15:36:26 +0300

    fix(ui): use standard bs3 pagination for ace theme

--- app/Views/pagers/bootstrap_full.php
+++ app/Views/pagers/bootstrap_full.php
@@ -1,7 +1,7 @@
 <?php $pager->setSurroundCount(2) ?>
 
 <nav aria-label="Page navigation">
-    <ul class="pagination pagination-sm justify-content-center mb-0 mt-3">
+    <ul class="pagination pagination">
         <?php if ($pager->hasPrevious()) : ?>
             <li class="page-item">
                 <a class="page-link" href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
